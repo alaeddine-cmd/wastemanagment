967e8e979e11f7e2dd45dd38959428f32f458f52
ala405 <127737904+ala405@users.noreply.github.com>
2024-01-28 16:03:08 +0100

    changes

---
+++ src/Components/TrashcanPage copy.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import emptyTrashcan from '../images/empty-trashcan.png';
+import crumpledPaper from '../images/crumpled-paper.png';
+
+const TrashcanPage = () => {
+  const [fillLevel, setFillLevel] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(fetchFillLevel, 2000); 
+
+    return () => clearInterval(intervalId); 
+  }, []);
+
+  const fetchFillLevel = async () => {
+    try {
+      const response = await fetch('http://192.168.1.13:8080/api/fillLevel/AB15620266');
+      if (!response.ok) {
+        throw new Error('Failed to fetch fill level');
+      }
+      const data = await response.json();
+      setFillLevel(data.fillLevel);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Trashcan System</h1>
+      <div style={{ position: 'relative', width: '200px', height: '300px', overflow: 'hidden' }}>
+        <img src={emptyTrashcan} alt="Empty Trashcan" style={{ width: '100%', height: '100%', position: 'absolute', top: '0', left: '0', zIndex: '1' }} />
+        {Array.from({ length: fillLevel }).map((_, index) => (
+          <img
+            key={index}
+            src={crumpledPaper}
+            alt="Crumpled Paper"
+            style={{
+              position: 'absolute',
+              width: '50%',
+              height: 'auto',
+              top: `${99 - (Math.floor(index / 3) + 1) * 20}%`,
+              left: `${(index % 3) * 22}%`,
+              transform: 'translate(7%, -50%)',
+              opacity: 5,
+              zIndex: '0',
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TrashcanPage;
